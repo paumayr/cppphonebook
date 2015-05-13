9ca365cdfba48b96686cf918a52821a175181204
Philipp Aumayr <philipp@software-architects.at>
2015-05-13 11:21:57 +0200

    step 2, menu loop

--- Phonebook/Phonebook/Phonebook.cpp
+++ Phonebook/Phonebook/Phonebook.cpp
@@ -6,6 +6,64 @@
 #include <iostream>
 #include <string>
 
+void printMenu()
+{
+	using namespace std;
+
+	cout << "menu:" << endl << "=====" << endl;
+
+	cout << "1) search" << endl;
+	cout << "2) add new" << endl;
+	cout << "3) update existing" << endl;
+	cout << "4) delete" << endl;
+	cout << "5) print all" << endl;
+	cout << "6) quit" << endl;
+}
+
+void newContact()
+{
+	using namespace std;
+
+	string first, last, number;
+	cout << "firstname?" << endl;
+	cin >> first;
+	cout << "lastname?" << endl;
+	cin >> last;
+	cout << "number?" << endl;
+	cin >> number;
+	cout << first << " " << last << " - " << number << endl;
+}
+
+void search()
+{
+	using namespace std;
+	cout << "search!" << endl;
+}
+
+void addNew()
+{
+	using namespace std;
+	cout << "addNew!" << endl;
+}
+
+void updateExisting()
+{
+	using namespace std;
+	cout << "updateExisting!" << endl;
+}
+
+void deleteEntry()
+{
+	using namespace std;
+	cout << "deleteEntry!" << endl;
+}
+
+void printAll()
+{
+	using namespace std;
+	cout << "printAll!" << endl;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	using namespace std;
@@ -14,17 +72,34 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	while (keepRunning)
 	{
-		string first, last, number;
-		cout << "firstname?" << endl;
-		cin >> first;
-		cout << "lastname?" << endl;
-		cin >> last;
-		cout << "number?" << endl;
-		cin >> number;
+		printMenu();
+		int choice;
+		cin >> choice;
 
-		cout << first << " " << last << " - " << number << endl;
+		switch (choice)
+		{
+		case 1: // search
+			search();
+			break;
+		case 2: // add new 
+			addNew();
+			break;
+		case 3: // update existing
+			updateExisting();
+			break;
+		case 4: // delete
+			deleteEntry();
+			break;
+		case 5: // print all
+			printAll();
+			break;
+		case 6: // quit
+			keepRunning = false;
+			break;
+		}
 	}
 
+	cout << "thank you for using phonebook." << endl;
 	return 0;
 }
 
